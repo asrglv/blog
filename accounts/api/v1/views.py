55a579d8be63d36a9fbe4ef155f826419aa77da4
asrglv <aktan.srgv@gmail.com>
2025-08-21 02:08:49 +0300

    Create views for User model in API v1: - Create UserPagination - Create UserModelViewSet

---
+++ accounts/api/v1/views.py
@@ -0,0 +1,29 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import NotFound
+from rest_framework.pagination import PageNumberPagination
+from accounts.models import User
+from .serializers import (UserReadSerializer,
+                          UserCreateSerializer,
+                          UserUpdateSerializer)
+
+
+class UserPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class UserModelViewSet(ModelViewSet):
+    pagination_class = UserPagination
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return UserReadSerializer
+        elif self.action == 'create':
+            return UserCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return UserUpdateSerializer
+        return NotFound('Method not allowed')
