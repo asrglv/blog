c1ae51c8ee0a1f13e6ebc546364d95f16efaf3c1
asrglv <aktan.srgv@gmail.com>
2025-08-20 23:05:48 +0300

    Add admin panel for accounts app: - Registered User model in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'name', 'surname', 'email', 'created_at',
+                    'updated_at', 'is_superuser']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['username', 'name', 'surname', 'email']
