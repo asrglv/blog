d3ba6a813c30cf22ed9b3f59fda68bea5b516731
asrglv <aktan.srgv@gmail.com>
2025-08-22 18:53:06 +0300

    Add admin panel for content app: - Registered Post model in admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from content.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'author', 'publish',
+                    'created_at', 'updated_at', 'status']
+    list_display_links = ['id', 'title', 'slug']
+    list_filter = ['status', 'publish', 'created_at', 'updated_at']
+    search_fields = ['id', 'title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
