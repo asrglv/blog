58015cd47bdecd83d6000e40ef3ae163e45aa7f6
asrglv <aktan.srgv@gmail.com>
2025-08-22 10:56:32 +0300

    Create models for content app: - Create model Post - Create manager PublishedManager

--- content/models.py
+++ content/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.utils.text import slugify
 
-# Create your models here.
+
+USER = settings.AUTH_USER_MODEL
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'draft', 'Draft'
+        PUBLISHED = 'published', 'Published'
+
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    author = models.ForeignKey(
+        USER,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    users_liked = models.ManyToManyField(USER, related_name='liked_posts')
+    users_disliked = models.ManyToManyField(USER, related_name='disliked_posts')
+    status = models.CharField(max_length=10,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug != slugify(self.title):
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
