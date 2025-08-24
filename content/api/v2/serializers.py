e4c0174b16ed6e8e03c0c728a246d9a87df6e5a4
asrglv <aktan.srgv@gmail.com>
2025-08-24 20:39:34 +0300

    Create serializer for Tag model in API v2 - Create TagSerializer - Integrate tags in fields for PostReadSerializer - Integrage tags in fields for PostCreateUpdateSerializer - Integrate tags functional in methods create, update for PostCreateUpdateSerializer

--- content/api/v2/serializers.py
+++ content/api/v2/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from content.models import Post
+from taggit.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 
 class PostReadSerializer(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
     author_username = serializers.SerializerMethodField()
     author_email = serializers.StringRelatedField(
         read_only=True,
@@ -11,15 +21,37 @@ class PostReadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'slug', 'author_id', 'author_username',
-                  'author_email', 'body', 'publish', 'created_at',
-                  'updated_at', 'users_liked', 'users_disliked', 'status']
+        fields = ['id', 'title', 'slug', 'author_id',
+                  'author_username', 'author_email', 'body',
+                  'publish', 'created_at', 'updated_at',
+                  'users_liked', 'users_disliked', 'tags', 'status']
 
     def get_author_username(self, obj):
         return obj.author.username
 
+    def get_tags(self, obj):
+        return TagSerializer(obj.tags.all(), many=True).data
+
 
 class PostCreateUpdateSerializer(serializers.ModelSerializer):
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all(),
+    )
+
     class Meta:
         model = Post
-        fields = ['title', 'author', 'body', 'status']
+        fields = ['title', 'author', 'body', 'tags', 'status']
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags')
+        post = super().create(validated_data)
+        if tags is not None:
+            post.tags.set(tags)
+        return post
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop('tags')
+        if tags is not None:
+            instance.tags.set(tags)
+        return super().update(instance, validated_data)
