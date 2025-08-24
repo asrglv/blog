b85626a9d1b4d5cf5bab6698c5e80b899827d548
asrglv <aktan.srgv@gmail.com>
2025-08-24 13:17:40 +0300

    Create viewset for Tag model in API v1: - Create TagPagination - Create TagViewSet

--- content/api/v1/views.py
+++ content/api/v1/views.py
@@ -2,13 +2,32 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import NotFound
 from rest_framework.pagination import PageNumberPagination
 from content.models import Post
-from .serializers import PostReadSerializer, PostCreateUpdateSerializer
+from taggit.models import Tag
+from .serializers import (PostReadSerializer,
+                          PostCreateUpdateSerializer,
+                          TagSerializer)
+
+
+class TagPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 
 class PostPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 20
+
+
+class TagViewSet(ModelViewSet):
+    pagination_class = TagPagination
+
+    def get_queryset(self):
+        return Tag.objects.all()
+
+    def get_serializer_class(self):
+        return TagSerializer
 
 
 class PostViewSet(ModelViewSet):
