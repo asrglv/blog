0654a6dd3164bd78c9c8942b0d11a308b8d535e8
asrglv <aktan.srgv@gmail.com>
2025-08-24 13:23:50 +0300

    Add url path for Tag in API v1 - Register TagViewSet in router

--- content/api/v1/urls.py
+++ content/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from .views import PostViewSet
+from .views import PostViewSet, TagViewSet
 
 
 router = SimpleRouter()
 router.register(r'posts', PostViewSet, basename='posts')
+router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('', include(router.urls)),
